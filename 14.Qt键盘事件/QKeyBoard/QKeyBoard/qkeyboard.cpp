5421099f135fe6234ffeba1289fc197fcd9b11dd
FengJungle <fengjungle@126.com>
2019-10-01 16:41:58 +0800

    Bug fix: add isAutoRepeat when keyevent is triggered, fix keyevent continuous and auto triggered

--- 14.Qt键盘事件/QKeyBoard/QKeyBoard/qkeyboard.cpp
+++ 14.Qt键盘事件/QKeyBoard/QKeyBoard/qkeyboard.cpp
@@ -1,10 +1,9 @@
 #include "qkeyboard.h"
 
 QKeyBoard::QKeyBoard(QWidget *parent, Qt::WFlags flags)
-	: QWidget(parent, flags)
+: QWidget(parent, flags)
 {
 	ui.setupUi(this);
-	
 }
 
 QKeyBoard::~QKeyBoard()
@@ -15,33 +14,51 @@ QKeyBoard::~QKeyBoard()
 void QKeyBoard::keyPressEvent(QKeyEvent *event){
 	switch(event->key()){
 		case Qt::Key_Escape:
-			this->ui.textEdit_press->append("Key_Escape Press");
+			if(!event->isAutoRepeat()){
+				this->ui.textEdit_press->append("Key_Escape Press");
+			}
 			break;
 		case Qt::Key_Tab:
-			this->ui.textEdit_press->append("Key_Tab Press");
+			if(!event->isAutoRepeat()){
+				this->ui.textEdit_press->append("Key_Tab Press");
+			}
 			break;
 		case Qt::Key_Enter:
-			this->ui.textEdit_press->append("Key_Enter Press");
+			if(!event->isAutoRepeat()){
+				this->ui.textEdit_press->append("Key_Enter Press");
+			}
 			break;
 		case Qt::Key_Delete:
-			this->ui.textEdit_press->append("Key_Delete Press");
+			if(!event->isAutoRepeat()){
+				this->ui.textEdit_press->append("Key_Delete Press");
+			}
 			break;
 		case Qt::Key_Space:
-			this->ui.textEdit_press->append("Key_Space Press");
+			if(!event->isAutoRepeat()){
+				this->ui.textEdit_press->append("Key_Space Press");
+			}
 			break;
 		case Qt::Key_Left:
-			this->ui.textEdit_press->append("Key_Left Press");
+			if(!event->isAutoRepeat()){
+				this->ui.textEdit_press->append("Key_Left Press");
+			}
 			break;
 		case Qt::Key_Up:
-			this->ui.textEdit_press->append("Key_Up Press");
+			if(!event->isAutoRepeat()){
+				this->ui.textEdit_press->append("Key_Up Press");
+			}
 			break;
 		case Qt::Key_Right:
-			this->ui.textEdit_press->append("Key_Right Press");
+			if(!event->isAutoRepeat()){
+				this->ui.textEdit_press->append("Key_Right Press");
+			}
 			break;
 		case Qt::Key_Down:
-			this->ui.textEdit_press->append("Key_Down Press");
+			if(!event->isAutoRepeat()){
+				this->ui.textEdit_press->append("Key_Down Press");
+			}
 			break;
-		/*default:
+			/*default:
 			this->ui.textEdit->append("KeyEvent");*/
 	}
 }
@@ -49,31 +66,49 @@ void QKeyBoard::keyPressEvent(QKeyEvent *event){
 void QKeyBoard::keyReleaseEvent(QKeyEvent *event){
 	switch(event->key()){
 		case Qt::Key_Escape:
-			this->ui.textEdit_release->append("Key_Escape Release");
+			if(!event->isAutoRepeat()){
+				this->ui.textEdit_release->append("Key_Escape Release");
+			}
 			break;
 		case Qt::Key_Tab:
-			this->ui.textEdit_release->append("Key_Tab Release");
+			if(!event->isAutoRepeat()){
+				this->ui.textEdit_release->append("Key_Tab Release");
+			}
 			break;
 		case Qt::Key_Enter:
-			this->ui.textEdit_release->append("Key_Enter Release");
+			if(!event->isAutoRepeat()){
+				this->ui.textEdit_release->append("Key_Enter Release");
+			}
 			break;
 		case Qt::Key_Delete:
-			this->ui.textEdit_release->append("Key_Delete Release");
+			if(!event->isAutoRepeat()){
+				this->ui.textEdit_release->append("Key_Delete Release");
+			}
 			break;
 		case Qt::Key_Space:
-			this->ui.textEdit_release->append("Key_Space Release");
+			if(!event->isAutoRepeat()){
+				this->ui.textEdit_release->append("Key_Space Release");
+			}
 			break;
 		case Qt::Key_Left:
-			this->ui.textEdit_release->append("Key_Left Release");
+			if(!event->isAutoRepeat()){
+				this->ui.textEdit_release->append("Key_Left Release");
+			}
 			break;
 		case Qt::Key_Up:
-			this->ui.textEdit_release->append("Key_Up Release");
+			if(!event->isAutoRepeat()){
+				this->ui.textEdit_release->append("Key_Up Release");
+			}
 			break;
 		case Qt::Key_Right:
-			this->ui.textEdit_release->append("Key_Right Release");
+			if(!event->isAutoRepeat()){
+				this->ui.textEdit_release->append("Key_Right Release");
+			}
 			break;
 		case Qt::Key_Down:
-			this->ui.textEdit_release->append("Key_Down Release");
+			if(!event->isAutoRepeat()){
+				this->ui.textEdit_release->append("Key_Down Release");
+			}
 			break;
 			/*default:
 			this->ui.textEdit->append("KeyEvent");*/
